382e49752bce81878f1a080b7b6c429566357ba8
Deni Mazzei <dconkell@gmail.com>
2021-08-25 18:23:27 -0500

    final updates to assignment

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ var currentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(currentTime);
 
 //timeblocks presented for each hour in the business day
-var timeBlock = $(".hour")
+var timeBlock = $(".hour");
 var now = parseInt(moment().format("H"));
 
 //each timeblock is color coded to indicate past/present/future
 $.each(timeBlock, function (i, hour) {
     var hourID = parseInt($(this).attr("id"));
     if (hourID === now) {
-        $(this).next().addClass ("present");
+        $(this).next().addClass("present");
     }else if (hourID < now) {
         $(this).next().addClass("past");
     }else if (hourId > now) {
@@ -28,55 +28,55 @@ $(".saveBtn").on("click", function (event) {
 
 //upon refresh, events persist
 $(document).ready(function () {
-    if(localStorage["9"] !==null && localStorage["9"] !== undfined) {
+    if(localStorage["9"] !==null && localStorage["9"] !== undefined) {
         var nineAm = $("<p>" + localStorage["9"] + "</p>");
         $("#nineAm").append(nineAm[0].innerText);
     }else {
         ("");
     }
-    if(localStorage["10"] !==null && localStorage["10"] !== undfined) {
+    if(localStorage["10"] !==null && localStorage["10"] !== undefined) {
         var tenAm = $("<p>" + localStorage["10"] + "</p>");
         $("#tenAm").append(tenAm[0].innerText);
     }else {
         ("");
     }
-    if(localStorage["11"] !==null && localStorage["11"] !== undfined) {
+    if(localStorage["11"] !==null && localStorage["11"] !== undefined) {
         var nineAm = $("<p>" + localStorage["11"] + "</p>");
         $("#elevAm").append(elevAm[0].innerText);
     }else {
         ("");
     }
-    if(localStorage["12"] !==null && localStorage["12"] !== undfined) {
+    if(localStorage["12"] !==null && localStorage["12"] !== undefined) {
         var twelvePm = $("<p>" + localStorage["12"] + "</p>");
         $("#twelvePm").append(twelvePm[0].innerText);
     }else {
         ("");
     }
-    if(localStorage["1"] !==null && localStorage["1"] !== undfined) {
+    if(localStorage["1"] !==null && localStorage["1"] !== undefined) {
         var onePm = $("<p>" + localStorage["1"] + "</p>");
         $("#onePm").append(onePm[0].innerText);
     }else {
         ("");
     }
-    if(localStorage["2"] !==null && localStorage["2"] !== undfined) {
+    if(localStorage["2"] !==null && localStorage["2"] !== undefined) {
         var twoPm = $("<p>" + localStorage["2"] + "</p>");
         $("#tw0Pm").append(twoPm[0].innerText);
     }else {
         ("");
     }
-    if(localStorage["3"] !==null && localStorage["3"] !== undfined) {
+    if(localStorage["3"] !==null && localStorage["3"] !== undefined) {
         var threePm = $("<p>" + localStorage["3"] + "</p>");
         $("#threePm").append(threePm[0].innerText);
     }else {
         ("");
     }
-    if(localStorage["4"] !==null && localStorage["4"] !== undfined) {
+    if(localStorage["4"] !==null && localStorage["4"] !== undefined) {
         var fourPm = $("<p>" + localStorage["4"] + "</p>");
         $("#fourPm").append(fourPm[0].innerText);
     }else {
         ("");
     }
-    if(localStorage["5"] !==null && localStorage["5"] !== undfined) {
+    if(localStorage["5"] !==null && localStorage["5"] !== undefined) {
         var fivePm = $("<p>" + localStorage["5"] + "</p>");
         $("#fivePm").append(fivePm[0].innerText);
     }else {
